670542bc17ac114a6766dc1e86dd0f94a947a73b
Snowden Fu <24203166+snowden-fu@users.noreply.github.com>
2024-01-12 20:12:50 +1100

    Add manifest.json, service-worker.js, and sidepanel.html files

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const pattern = 'https:\/\/www\.seek\.com\.au\/job\/.*';
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+  if (!tab.url) return;
+  const url = new URL(tab.url);
+  // Enables the side panel on string matches pattern
+  if (url.href.match(pattern)) {
+    await chrome.sidePanel.setOptions({
+      tabId,
+      path: 'sidepanel.html',
+      enabled: true
+    });
+  } else {
+    // Disables the side panel on all other sites
+    await chrome.sidePanel.setOptions({
+      tabId,
+      enabled: false
+    });
+  }
+});
