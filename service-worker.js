f2a20171012e39779232094bf7deb4bd38a8f678
Snowden Fu <24203166+snowden-fu@users.noreply.github.com>
2024-01-12 20:38:18 +1100

    Refactor service-worker.js and add new functions for PDF conversion and job analysis

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const pattern = 'https:\/\/www\.seek\.com\.au\/job\/.*';
+const pattern = "https://www.seek.com.au/job/.*";
 
 chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
   if (!tab.url) return;
@@ -7,14 +7,49 @@ chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
   if (url.href.match(pattern)) {
     await chrome.sidePanel.setOptions({
       tabId,
-      path: 'sidepanel.html',
-      enabled: true
+      path: "sidepanel.html",
+      enabled: true,
     });
   } else {
     // Disables the side panel on all other sites
     await chrome.sidePanel.setOptions({
       tabId,
-      enabled: false
+      enabled: false,
     });
   }
 });
+
+function getResumePdfToTxt(getResumePdf) {
+  return fetch("http://localhost:5000/convert", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      resume: getResumePdf,
+    }),
+  }).then((res) => res.json());
+}
+function getJobDetailTxt(jobDetailUrl) {
+  return fetch("http://localhost:5000/job", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      job_detail_url: jobDetailUrl,
+    }),
+  }).then((res) => res.json());
+}
+function getJobAnalysis(jobDetailTxt, resumeTxt) {
+  return fetch("http://localhost:5000/analyze", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      job_detail: jobDetailTxt,
+      resume: resumeTxt,
+    }),
+  }).then((res) => res.json());
+}
