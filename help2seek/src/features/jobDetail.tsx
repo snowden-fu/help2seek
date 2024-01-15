f4b860773dbfb30486f76d1be7119a6bb2902b0e
Snowden Fu <24203166+snowden-fu@users.noreply.github.com>
2024-01-15 12:26:24 +1100

    add JobDetail to sidepanel

---
+++ help2seek/src/features/jobDetail.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+
+const JobDetail: React.FC = () => {
+  return (
+    <div>
+      <h1>AI analysis</h1>
+      <section>
+        <h2>Overall Score</h2>
+        <p>80%</p>
+      </section>
+      <section>
+        <h2>visa check</h2>
+        <p>PR required</p>
+      </section>
+      <section>
+        <h2>Skills matched</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>PHP</li>
+          <li>MySQL</li>
+        </ul>
+      </section>
+      <section>
+        <h2>skill missed</h2>
+        <ul>
+          <li>Java</li>
+          <li>Python</li>
+          <li>SQL</li>
+        </ul>
+      </section>
+    </div>
+  )
+}
+
+export default JobDetail
---
+++ help2seek/src/sidepanel/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import JobDetail from '~features/jobDetail';
+
+const IndexSidePanel: React.FC = () => {
+    return (
+        <div>
+            <JobDetail />
+        </div>
+    );
+};
+
+export default IndexSidePanel;
