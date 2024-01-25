a311d8f02b578d4db5956d60ad3094b40336a0b3
Snowden Fu <24203166+snowden-fu@users.noreply.github.com>
2024-01-25 21:20:42 +1100

    Add uploadResume function to background index.ts

--- help2seek/src/background/index.ts
+++ help2seek/src/background/index.ts
@@ -3,3 +3,22 @@ export {}
 console.log(
   "Live now; make now always the most precious time. Now will never come again."
 )
+function uploadResume(resume:File) {
+  console.log(`uploadResume ${resume.name} ${resume.size} ${resume.type}`)
+  let formData = new FormData()
+  formData.append("resume", resume)
+  const api_url = process.env.RESUME_PARSING_API
+  console.log(`api_url ${api_url}`)
+  fetch(api_url, {
+    method: "POST",
+    body: formData
+  })
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  
+}
