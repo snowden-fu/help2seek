3805d9d637ce918c493b1437fbd0fa562339f581
Snowden Fu <24203166+snowden-fu@users.noreply.github.com>
2024-01-26 10:56:59 +1100

    Add functions for job detail retrieval and analysis

--- help2seek/src/background/index.ts
+++ help2seek/src/background/index.ts
@@ -22,3 +22,48 @@ function uploadResume(resume:File) {
     })
   
 }
+/**
+ * get job detail from seek detail page,
+ * trigger by tab url match
+ * saved in session storage
+ * @param url url of job detail page, should be from tab.url
+ */
+function get_job_detail(url:string) {
+  console.log(`get_job_detail url ${url}`)
+  const api_url = process.env.JOB_DETAIL_API
+  console.log(`api_url ${api_url}`)
+  fetch(api_url, {
+    method: "POST",
+    body: JSON.stringify({ url: url })
+  })
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+
+/**
+ * analyze job detail and resume, 
+ * job detail and resume should both submitted from local to server for asessment
+ * @param resume resume text parsed from pdf
+ * @param job_detail job detail text parsed from seek job detail page, saved in session storage
+ */
+function analyze_job_detail(resume:string, job_detail:string) {
+  const api_url = process.env.JOB_ANALYSIS_API
+  console.log(`api_url ${api_url}`)
+  fetch(api_url, {
+    method: "POST",
+    body: JSON.stringify({ resume_text: resume, job_desc: job_detail })
+  })
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
