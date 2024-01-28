a7f287c36cd0b9a060b9dbb440b2bafd4d023eb4
Snowden Fu <24203166+snowden-fu@users.noreply.github.com>
2024-01-29 10:53:55 +1100

    get job_desc from data

--- help2seek/src/background/index.ts
+++ help2seek/src/background/index.ts
@@ -1,11 +1,11 @@
-import { json } from 'stream/consumers'
+import { json } from "stream/consumers"
 
 export {}
- 
+
 console.log(
   "Live now; make now always the most precious time. Now will never come again."
 )
-function uploadResume(resume:File) {
+function uploadResume(resume: File) {
   console.log(`uploadResume ${resume.name} ${resume.size} ${resume.type}`)
   let formData = new FormData()
   formData.append("resume", resume)
@@ -15,14 +15,13 @@ function uploadResume(resume:File) {
     method: "POST",
     body: formData
   })
-    .then(res => res.json())
-    .then(res => {
+    .then((res) => res.json())
+    .then((res) => {
       console.log(res)
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err)
     })
-  
 }
 /**
  * get job detail from seek detail page,
@@ -30,40 +29,38 @@ function uploadResume(resume:File) {
  * saved in session storage
  * @param url url of job detail page, should be from tab.url
  */
-function get_job_detail(url:string) {
+function get_job_detail(url: string) {
   console.log(`get_job_detail url ${url}`)
-  const api_url = "https://australia-southeast1-help2seek.cloudfunctions.net/fetch_seek_job_details"
+  const api_url =
+    "https://australia-southeast1-help2seek.cloudfunctions.net/fetch_seek_job_details"
   console.log(`api_url ${api_url}`)
-  fetch(api_url, {
+  return fetch(api_url, {
     method: "POST",
     body: JSON.stringify({ job_url: url }),
     headers: {
       "Content-Type": "application/json"
     }
-  }).then(res => {
-    console.log(res.json())
   })
 }
 
-
 /**
- * analyze job detail and resume, 
+ * analyze job detail and resume,
  * job detail and resume should both submitted from local to server for asessment
  * @param resume resume text parsed from pdf
  * @param job_detail job detail text parsed from seek job detail page, saved in session storage
  */
-function analyze_job_detail(resume:string, job_detail:string) {
+function analyze_job_detail(resume: string, job_detail: string) {
   const api_url = process.env.JOB_ANALYSIS_API
   console.log(`api_url ${api_url}`)
   fetch(api_url, {
     method: "POST",
     body: JSON.stringify({ resume_text: resume, job_desc: job_detail })
   })
-    .then(res => res.json())
-    .then(res => {
+    .then((res) => res.json())
+    .then((res) => {
       console.log(res)
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err)
     })
 }
@@ -71,19 +68,26 @@ const url_pattern = /https:\/\/www.seek.com.au\/job\/.*/
 function checkTabURL(tabId: number) {
   chrome.tabs.get(tabId, (tab) => {
     if (tab.url && url_pattern.test(tab.url)) {
-      console.log("URL matches the pattern:", tab.url);
-      // Add any additional logic here
-      get_job_detail(tab.url)
+      console.log("URL matches the pattern:", tab.url)
+      get_job_detail(tab.url).then((res) => {
+        return res.json()
+      })
+      .then(data=>{
+        console.log(data["job_desc"]);
+        // if resume text is in storage
+        // get resume text out of storage
+        // call analyze_job_detail
+      })
     }
-  });
+  })
 }
 
-chrome.tabs.onActivated.addListener(activeInfo => {
-  checkTabURL(activeInfo.tabId);
-});
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  checkTabURL(activeInfo.tabId)
+})
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete') {
-    checkTabURL(tabId);
+  if (changeInfo.status === "complete") {
+    checkTabURL(tabId)
   }
-});
+})
